953ca234fa1ed8852b7663d1e97e2f9e3f450b2c
Gregory Benner <gregbenner1@gmail.com>
2024-05-09 21:36:27 -0400

    try to get rgbled working for lily58/light

--- keyboards/lily58/keymaps/klathmon_light/config.h
+++ keyboards/lily58/keymaps/klathmon_light/config.h
@@ -1,3 +1,22 @@
 #pragma once
 
 #include "../klathmon/config.h" // IWYU pragma: keep
+
+#ifdef RGBLIGHT_ENABLE
+#    undef RGBLIGHT_EFFECT_BREATHING
+#    undef RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    undef RGBLIGHT_EFFECT_SNAKE
+#    undef RGBLIGHT_EFFECT_KNIGHT
+#    undef RGBLIGHT_EFFECT_CHRISTMAS
+#    undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    undef RGBLIGHT_EFFECT_RGB_TEST
+#    undef RGBLIGHT_EFFECT_ALTERNATING
+#    undef RGBLIGHT_EFFECT_TWINKLE
+
+#    define RGBLIGHT_DEFAULT_SAT 0
+#    define RGBLIGHT_DEFAULT_VAL 16
+#    define RGBLIGHT_DEFAULT_SPD 120
+#    define RGBLIGHT_SPLIT
+#    define RGBLIGHT_SLEEP //  the RGB lighting will be switched off when the host goes to sleep
+#endif
--- keyboards/lily58/keymaps/klathmon_light/rules.mk
+++ keyboards/lily58/keymaps/klathmon_light/rules.mk
@@ -1,12 +1,9 @@
 USER_NAME := klathmon
 BOOTLOADER = atmel-dfu
 
-BOOTMAGIC_ENABLE  = no  # Enable Bootmagic Lite
-BACKLIGHT_ENABLE  = no  # Enable keyboard backlight functionality
-AUDIO_ENABLE      = no  # Audio output
-RGBLIGHT_ENABLE   = yes # Enable WS2812 RGB underlight.
-SWAP_HANDS_ENABLE = no  # Enable one-hand typing
 ENCODER_ENABLE    = yes # Enable rotary encoder
 OLED_ENABLE       = yes # OLED display
+DYNAMIC_MACRO_ENABLE = yes # Enable Dynamic Macros
+RGBLIGHT_ENABLE   = no # Enable WS2812 RGB underlight.
 
 SRC += ./lib/logo_reader.c
