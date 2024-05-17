e832ce0643bbe7be191f82fc67e3e63b11121726
Gregory Benner <gregbenner1@gmail.com>
2024-05-16 23:17:13 -0400

    replace submodules with simple install and compile-all shell scripts

---
+++ compile-all.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+qmk clean -a
+qmk userspace-compile -j 0
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+qmk setup -H "$(pwd)/qmk_firmware" Klathmon/qmk_firmware -b klathmon_external_userspace -y
