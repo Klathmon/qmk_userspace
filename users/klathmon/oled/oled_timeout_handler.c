4f85907537b54f377e25b8112c63c1a99b4dedc4
Gregory Benner <gbenner@dropbox.com>
2024-05-30 13:38:51 -0700

    allow using a 16 bit timer to save some stack space if needed

--- users/klathmon/oled/oled_timeout_handler.c
+++ users/klathmon/oled/oled_timeout_handler.c
@@ -4,17 +4,29 @@
 #    include "print.h"
 #endif
 
-static uint32_t oled_timer  = 0;
-static bool     oled_active = true; // Added flag to track the OLED state
+// #define USE_16_BIT_TIMER // When defined it will use 16 bit timers instead of 32 bit timers, saving a bit of space and stack size
+
+#ifdef USE_16_BIT_TIMER
+#    define OLED_TIMER_SIZE uint16_t
+#    define TIMER_READ_FN timer_read
+#    define TIMER_ELAPSED_FN timer_elapsed
+#else
+#    define OLED_TIMER_SIZE uint32_t
+#    define TIMER_READ_FN timer_read32
+#    define TIMER_ELAPSED_FN timer_elapsed32
+#endif
+
+static OLED_TIMER_SIZE oled_timer  = 0;
+static bool            oled_active = true; // Added flag to track the OLED state
 
 bool oled_handle_timeout(void) {
     if (oled_timer == 0) {
-        oled_timer = timer_read32(); // Initialize timer at first use
+        oled_timer = TIMER_READ_FN(); // Initialize timer at first use
     }
 
     // Update the timer if there's activity, or timeout the screen if there's no activity
-    if (timer_elapsed32(last_input_activity_time()) < OLED_TIMEOUT) {
-        oled_timer = timer_read32();
+    if (TIMER_ELAPSED_FN(last_input_activity_time()) < OLED_TIMEOUT) {
+        oled_timer = TIMER_READ_FN();
         if (!oled_active) { // If the OLED was off, turn it back on due to activity
 #ifdef CONSOLE_ENABLE
             // Log the time elapsed for debugging
@@ -23,12 +35,12 @@ bool oled_handle_timeout(void) {
             oled_on();
             oled_active = true;
         }
-    } else if (timer_elapsed32(oled_timer) >= OLED_TIMEOUT) {
+    } else if (TIMER_ELAPSED_FN(oled_timer) >= OLED_TIMEOUT) {
         if (oled_active) { // Only turn off the OLED if it is currently on
             oled_off();
 #ifdef CONSOLE_ENABLE
             // Log the time elapsed for debugging
-            uprintf("Disabling OLED after %d ms\n", timer_elapsed32(oled_timer));
+            uprintf("Disabling OLED after %d ms\n", TIMER_ELAPSED_FN(oled_timer));
 #endif
             oled_active = false;
         }
