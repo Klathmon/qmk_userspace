a20eb60535a43ab5e00f3b86c25c4b48ed5bf343
Gregory Benner <gbenner@dropbox.com>
2024-05-30 12:43:01 -0700

    save a bit more space in the wpm module

--- users/klathmon/oled/wpm_animation.h
+++ users/klathmon/oled/wpm_animation.h
@@ -3,13 +3,13 @@
 #include "../klathmon.h" // IWYU pragma: keep
 
 #ifdef OLED_WPM_GRAPH
-// #    define SHOW_WPM_TEXT              /* if defined, the WPM text will be printed on the screen */
+// #    define SHOW_WPM_TEXT              /* if defined, the WPM text will be printed on the screen, disabling this saves a lot of space */
 #    define GRAPH_TOP_WPM 110.0f       /* WPM value at the top of the graph window */
 #    define GRAPH_REFRESH_INTERVAL 100 /* in milliseconds */
 #    define GRAPH_AREA_FILL_INTERVAL 3 /* determines how dense the horizontal lines under the graph line are; lower = more dense */
 #    define VERT_INTERVAL 3            /* determines frequency of vertical lines under the graph line */
-#    define VERT_LINE                  /* if defined, it will draw vertical lines */
-#    define GRAPH_LINE_THICKNESS 3     /* determines thickness of graph line in pixels */
+// #    define VERT_LINE                  /* if defined, it will draw vertical lines, disabling this saves a small amount of firmware space */
+#    define GRAPH_LINE_THICKNESS 3 /* determines thickness of graph line in pixels */
 #endif
 
 #ifdef OLED_STATIC_BUILDUP
