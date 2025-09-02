2a537fbeab81710e283307fc55c643d26cfed4f5
Gregory Benner <gregbenner1@gmail.com>
2025-09-01 20:57:19 -0400

    fix israfel keymap

--- keyboards/israfel/keymaps/klathmon/keymap.c
+++ keyboards/israfel/keymaps/klathmon/keymap.c
@@ -23,65 +23,65 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //                                                         |                                                       //
     [_MAIN_WIN] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        M_HESCW,  M_LALTE,  KC_LCTL,       KC_SPC,                   KC_DEL,       M_MVMTW,  M_NUMP,  M_HESCW
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
+        KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+        M_HESCW,  M_LALTE,  KC_LCTL,       KC_SPC,                   KC_DEL,       M_MVMTW,  M_NUMP,  M_HESCW, KC_BSLS
     ),
     //                                                         |                                                       //
     [_MVMT_WIN] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_HOME, KC_TRNS, KC_END,  KC_MINS, KC_EQL,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, M_LWRDW, KC_UP,   M_RWRDW, KC_LBRC, KC_RBRC,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_LCBR, KC_RCBR,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_HOME, KC_TRNS, KC_END,  KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS,                  KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, M_LWRDW, KC_UP,   M_RWRDW, KC_LBRC, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_LCBR,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_HOME, KC_TRNS, KC_END,  KC_TRNS, KC_RBRC,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS,                  KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS, KC_RCBR
     ),
     //                                                         |                                                       //
     [_MAIN_MAC] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        M_MSPOT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        M_HESCM,  M_LALTE,  KC_LGUI,       KC_SPC,                   KC_DEL,       M_MVMTM,  M_NUMP,  M_HESCM
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
+        M_MSPOT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+        M_HESCM,  M_LALTE,  KC_LGUI,       KC_SPC,                   KC_DEL,       M_MVMTM,  M_NUMP,  M_HESCM, KC_BSLS
     ),
     //                                                         |                                                       //
     [_MVMT_MAC] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, M_HOMEM, KC_TRNS, M_ENDM,  KC_MINS, KC_EQL,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, M_LWRDM, KC_UP,   M_RWRDM, KC_LBRC, KC_RBRC,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_LCBR, KC_RCBR,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, M_HOMEM, KC_TRNS, M_ENDM , KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS,                  KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, M_LWRDM, KC_UP,   M_RWRDM, KC_LBRC, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_LCBR,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, M_HOMEM, KC_TRNS, M_ENDM , KC_TRNS, KC_RBRC,
+        KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS,                  KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS, KC_RCBR
     ),
     //                                                         |                                                       //
     [_NUMR] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS,                  KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS
+        KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS,                  KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS
     ),
     //                                                         |                                                       //
     [_NUMP] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, KC_TRNS, KC_TRNS,     KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_TRNS,
         KC_TRNS, KC_TRNS, RGB_HUD, RGB_VAI, RGB_HUI, KC_TRNS,     KC_TRNS, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_TRNS,
-        KC_TRNS, KC_TRNS, RGB_SAD, RGB_VAD, RGB_SAI, KC_TRNS,     KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_PENT, KC_TRNS,
+        KC_TRNS, KC_TRNS, RGB_SAD, RGB_VAD, RGB_SAI, KC_TRNS,     KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_PENT,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_P0,   KC_P0,   KC_PDOT, KC_PENT, KC_TRNS,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS,                  KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS
+        KC_TRNS,  KC_TRNS,  KC_TRNS,       KC_TRNS,                  KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS
     ),
     //                                                         |                                                       //
     [_HYPR_WIN] = LAYOUT(
         KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_TRNS,     QK_MAKE, KC_BRMU, KVM_MA4, KVM_MA1, KVM_MA2, KVM_MA3,
         KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TRNS,     KC_PSCR, KC_BRMD, KVM_KBM, KVM_SCB, KVM_SCC, KVM_SCA,
-        KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,     M_LOCKW, KC_VOLD, KC_MUTE, KC_VOLU, KC_WBAK, KC_WFWD,
+        KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,     M_LOCKW, KC_VOLD, KC_MUTE, KC_VOLU, KC_WBAK,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     M_SLEPW, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, M_TGLM,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,       QK_RBT,                   QK_BOOT,      KQ_FLSH,  KC_TRNS, KC_TRNS
+        KC_TRNS,  KC_TRNS,  KC_TRNS,       QK_RBT,                   QK_BOOT,      KQ_FLSH,  KC_TRNS, KC_TRNS, KC_WFWD
     ),
     [_HYPR_MAC] = LAYOUT(
         KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_TRNS,     QK_MAKE, KC_BRMU, KVM_MA4, KVM_MA1, KVM_MA2, KVM_MA3,
         KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TRNS,     KC_PSCR, KC_BRMD, KVM_KBM, KVM_SCB, KVM_SCC, KVM_SCA,
-        KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,     M_LOCKW, KC_VOLD, KC_MUTE, KC_VOLU, M_BAKM,  M_FWDM,
+        KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,     M_LOCKW, KC_VOLD, KC_MUTE, KC_VOLU, M_BAKM,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     M_SLEPW, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, M_TGLM,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,       QK_RBT,                   QK_BOOT,      KQ_FLSH,  KC_TRNS, KC_TRNS
+        KC_TRNS,  KC_TRNS,  KC_TRNS,       QK_RBT,                   QK_BOOT,      KQ_FLSH,  KC_TRNS, KC_TRNS, M_FWDM
     )
 };
 // clang-format on
