ecef40dc980306e22b68794ee0adad693cf744c4
Gregory Benner <gregbenner1@gmail.com>
2026-03-22 10:22:10 -0400

    feat: add helix rev3_5rows with Elite Pi support
    - Port lumberjack keymap with lily58 inner keys (mute, lock, sleep)
    - Configure CONVERT_TO=elite_pi for RP2040 controllers
    - Disable OLED, RGB, and encoder (basic build)
    - Bump qmk.json to userspace v1.1 for env var support

---
+++ keyboards/helix/keymaps/klathmon/rules.mk
@@ -0,0 +1,10 @@
+USER_NAME := klathmon
+
+CONVERT_TO = elite_pi
+
+OLED_ENABLE       = no  # no screen
+RGBLIGHT_ENABLE   = no  # no RGB underglow
+RGB_MATRIX_ENABLE = no  # no RGB matrix
+ENCODER_ENABLE    = no  # no rotary encoder
+
+include ../users/klathmon/rules.mk # This is only included here is to make the generate-compilation-database command work properly.
